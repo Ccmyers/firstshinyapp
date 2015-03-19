2eeabaceb177b3aaab0ef78d41e7c31da283d60d
Chris Myers <cmyers@microsoft.com>
2015-03-19 16:38:01 -0700

    server and ui files for shiny app

---
+++ ui.R
@@ -0,0 +1,44 @@
+library(shiny)
+
+# Define UI for random distribution application 
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Tabsets updated by Chris Myers"),
+  
+  # Sidebar with controls to select the random distribution type
+  # and number of observations to generate. Note the use of the
+  # br() element to introduce extra vertical spacing
+  sidebarLayout(
+    sidebarPanel(
+      # caption that can be updated
+      textInput("caption", "Caption:", "Data Distributions"),
+      
+      # selecting which distribution
+      radioButtons("dist", "Distribution type:",
+                   c("Normal" = "norm",
+                     "Uniform" = "unif",
+                     "Log-normal" = "lnorm",
+                     "Exponential" = "exp")),
+      br(),
+      
+      #slider input
+      sliderInput("n", 
+                  "Number of observations:", 
+                  value = 500,
+                  min = 1, 
+                  max = 1000)
+    ),
+    
+    # Show a tabset that includes a plot, summary, and table view
+    # of the generated distribution
+    mainPanel(
+      h3(textOutput("caption", container = span)),
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Plot", plotOutput("plot")), 
+                  tabPanel("Summary", verbatimTextOutput("summary")), 
+                  tabPanel("Table", tableOutput("table"))
+      )
+    )
+  )
+))
